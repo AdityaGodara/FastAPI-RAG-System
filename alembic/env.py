e0b244eba40073193cc1f36dc9619a8ef9b525f2
AdityaGodara <adityagodara03@gmail.com>
2026-07-06 22:44:05 +0530

    Alembic error here

--- alembic/env.py
+++ alembic/env.py
@@ -7,9 +7,20 @@ from alembic import context
 
 from app.db.base import Base
 
+from app.core.config import settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.database_url.replace("postgresql+asyncpg", "postgresql+psycopg")
+)
+
+print("="*80)
+print(f"Using database URL: {config.get_main_option('sqlalchemy.url')}")
+print("="*80)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,7 +31,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
