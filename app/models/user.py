220527a4f3c211afe25e691c80a9bcebe38b6de6
AdityaGodara <adityagodara03@gmail.com>
2026-07-06 21:43:33 +0530

    Added database config and users table

--- app/models/user.py
+++ app/models/user.py
@@ -1,22 +1,24 @@
-from app.models.base import Base
+import uuid
+
+from app.db.base import Base
 
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import UUID, DateTime, func
+from sqlalchemy import UUID, DateTime, String, func, Boolean
 
 class User(Base):
     __tablename__ = "users"
 
-    id : Mapped[UUID] = mapped_column(primary_key=True)
+    id : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),primary_key=True, nullable=False, default=uuid.uuid4)
 
-    name: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(String(225),nullable=False)
 
-    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
 
-    password_hash: Mapped[str] = mapped_column(nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(225), nullable=False)
 
-    is_active: Mapped[bool] = mapped_column(nullable=False, default=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
 
-    is_verified: Mapped[bool] = mapped_column(nullable=False, default=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
 
     created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), 
                                                  server_default=func.now(), 
